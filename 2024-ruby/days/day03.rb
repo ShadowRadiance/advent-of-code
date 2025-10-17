8d92e3b31c548e1689f119034cb2448a936486ff
Megan McVey <megan@offbyone.ca>
2025-10-17 18:55:28 -0300

    Day 03 Part A

--- 2024-ruby/days/day03.rb
+++ 2024-ruby/days/day03.rb
@@ -6,12 +6,29 @@ module Days
       @puzzle_input = puzzle_input
     end
 
+    RX_GOOD_MUL = /mul\(\d+,\d+\)/
+    MUL_VALUES = /mul\((\d+),(\d+)\)/
+
     def part_a
-      "PENDING-A"
+      puzzle_input.scan(RX_GOOD_MUL)
+                  .map { |mul| perform_mul(mul) }
+                  .sum
+                  .to_s
     end
 
     def part_b
       "PENDING-B"
+    end
+
+    def parse_input
+      puzzle_input
+    end
+
+    def perform_mul(mul)
+      mul.scan(MUL_VALUES)
+         .first
+         .map(&:to_i)
+         .reduce(:*)
     end
 
     private
--- 2024-ruby/spec/days/day03_spec.rb
+++ 2024-ruby/spec/days/day03_spec.rb
@@ -5,12 +5,18 @@ require_relative "../../days/day03"
 RSpec.describe "Days::Day03" do
   let(:day_runner) { Days::Day03.new(puzzle_input) }
 
-  let(:puzzle_input) { "" }
+  let(:puzzle_input) { "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))" }
+
+  describe ".parse_input" do
+    it "returns a list of reports with levels as integers" do
+      expect(day_runner.parse_input).to eq(puzzle_input)
+    end
+  end
 
   describe "#part_a" do
     context "with the sample data" do
       it "returns the correct value for part A" do
-        expect(day_runner.part_a).to eq "PENDING-A"
+        expect(day_runner.part_a).to eq "161"
       end
     end
   end
@@ -18,7 +24,7 @@ RSpec.describe "Days::Day03" do
   describe "#part_b" do
     context "with the sample data" do
       it "returns the correct value for part B" do
-        expect(day_runner.part_b).to eq "PENDING-B"
+        expect(day_runner.part_b).to eq "48"
       end
     end
   end
