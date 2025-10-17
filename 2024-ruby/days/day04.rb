bba5a797d366530b72f670bc974f9d40431cfc1a
Megan McVey <megan@offbyone.ca>
2025-10-17 19:24:50 -0300

    Day 04 Setup

---
+++ 2024-ruby/days/day04.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Days
+  class Day04
+    def initialize(puzzle_input)
+      @puzzle_input = puzzle_input
+    end
+
+    def part_a
+      "PENDING-A"
+    end
+
+    def part_b
+      "PENDING-B"
+    end
+
+    private
+
+    attr_reader :puzzle_input
+  end
+end
