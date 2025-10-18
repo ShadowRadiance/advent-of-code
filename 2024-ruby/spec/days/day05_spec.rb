f2a7d19c546e726e517467cc07272f778f83f7b2
Megan McVey <megan@offbyone.ca>
2025-10-18 18:39:43 -0300

    Day 05 Setup

---
+++ 2024-ruby/spec/days/day05_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require_relative "../../days/day05"
+
+RSpec.describe "Days::Day05" do
+  let(:day_runner) { Days::Day05.new(puzzle_input) }
+
+  let(:puzzle_input) { "" }
+
+  describe "#part_a" do
+    context "with the sample data" do
+      it "returns the correct value for part A" do
+        expect(day_runner.part_a).to eq "PENDING-A"
+      end
+    end
+  end
+
+  describe "#part_b" do
+    context "with the sample data" do
+      it "returns the correct value for part B" do
+        expect(day_runner.part_b).to eq "PENDING-B"
+      end
+    end
+  end
+end
