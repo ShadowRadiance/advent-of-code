6989980a301418b1174f73e619159920ff7f5bf2
Megan McVey <megan@offbyone.ca>
2025-10-19 09:39:13 -0300

    Day 05 Part A

--- 2024-ruby/days/day05.rb
+++ 2024-ruby/days/day05.rb
@@ -7,11 +7,83 @@ module Days
     end
 
     def part_a
-      "PENDING-A"
+      # score the correctly ordered updates
+
+      updates = parse_input[:updates]
+      rules = parse_input[:rules]
+
+      raise "PAGE PRINTED TWICE IN AN UPDATE" if updates.any? do |update|
+        update.uniq.length != update.length
+      end
+
+      updates.filter { update_correctly_ordered?(it, rules) }
+             .map { update_score(it) }
+             .sum.to_s
     end
 
     def part_b
       "PENDING-B"
+    end
+
+    def update_score(update)
+      middle_index = (update.length - 1) / 2
+      update[middle_index]
+    end
+
+    def update_correctly_ordered?(update, rules)
+      rules.each do |rule|
+        next unless rule_applies?(update, rule)
+
+        return false if rule_broken?(update, rule)
+      end
+
+      true
+    end
+
+    def rule_applies?(update, rule)
+      [rule[:earlier], rule[:later]].all? { update.include?(it) }
+    end
+
+    def rule_broken?(update, rule)
+      earlier_index = update.find_index(rule[:earlier])
+      later_index = update.find_index(rule[:later])
+
+      earlier_index > later_index
+    end
+
+    # ---
+
+    def parse_input
+      result = {
+        rules: [],
+        updates: [],
+      }
+      parser = RuleParser.new
+      puzzle_input.lines(chomp: true).each do |line|
+        if line.empty?
+          parser = UpdateParser.new
+          next
+        end
+        result[parser.key] << parser.parse(line)
+      end
+      result
+    end
+
+    class RuleParser
+      def parse(line)
+        earlier, later = line.split("|")
+        { earlier: earlier.to_i, later: later.to_i }
+      end
+
+      def key = :rules
+    end
+
+    class UpdateParser
+      def parse(line)
+        line.split(",").map(&:to_i)
+      end
+
+      def key = :updates
     end
 
     private
--- 2024-ruby/spec/days/day05_spec.rb
+++ 2024-ruby/spec/days/day05_spec.rb
@@ -5,12 +5,83 @@ require_relative "../../days/day05"
 RSpec.describe "Days::Day05" do
   let(:day_runner) { Days::Day05.new(puzzle_input) }
 
-  let(:puzzle_input) { "" }
+  let(:puzzle_input) do
+    <<~INPUT
+      47|53
+      97|13
+      97|61
+      97|47
+      75|29
+      61|13
+      75|53
+      29|13
+      97|29
+      53|29
+      61|53
+      97|53
+      61|29
+      47|13
+      75|47
+      97|75
+      47|61
+      75|61
+      47|29
+      75|13
+      53|13
+
+      75,47,61,53,29
+      97,61,53,29,13
+      75,29,13
+      75,97,47,61,53
+      61,13,29
+      97,13,75,29,47
+    INPUT
+  end
+
+  describe ".parse_input" do
+    it "returns a hash with the rules and updates" do
+      expect(day_runner.parse_input).to eq(
+        {
+          rules: [
+            { earlier: 47, later: 53 },
+            { earlier: 97, later: 13 },
+            { earlier: 97, later: 61 },
+            { earlier: 97, later: 47 },
+            { earlier: 75, later: 29 },
+            { earlier: 61, later: 13 },
+            { earlier: 75, later: 53 },
+            { earlier: 29, later: 13 },
+            { earlier: 97, later: 29 },
+            { earlier: 53, later: 29 },
+            { earlier: 61, later: 53 },
+            { earlier: 97, later: 53 },
+            { earlier: 61, later: 29 },
+            { earlier: 47, later: 13 },
+            { earlier: 75, later: 47 },
+            { earlier: 97, later: 75 },
+            { earlier: 47, later: 61 },
+            { earlier: 75, later: 61 },
+            { earlier: 47, later: 29 },
+            { earlier: 75, later: 13 },
+            { earlier: 53, later: 13 },
+          ],
+          updates: [
+            [75, 47, 61, 53, 29],
+            [97, 61, 53, 29, 13],
+            [75, 29, 13],
+            [75, 97, 47, 61, 53],
+            [61, 13, 29],
+            [97, 13, 75, 29, 47],
+          ],
+        },
+      )
+    end
+  end
 
   describe "#part_a" do
     context "with the sample data" do
       it "returns the correct value for part A" do
-        expect(day_runner.part_a).to eq "PENDING-A"
+        expect(day_runner.part_a).to eq "143"
       end
     end
   end
