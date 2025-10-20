47fd285d1c280cd9e0a95738674d2dba81b0b96b
Megan McVey <megan@offbyone.ca>
2025-10-20 16:28:44 -0300

    Day 07 Part A

--- 2024-ruby/days/day07.rb
+++ 2024-ruby/days/day07.rb
@@ -6,8 +6,74 @@ module Days
       @puzzle_input = puzzle_input
     end
 
+    def parse_input
+      puzzle_input.lines(chomp: true).map do |line|
+        parse_line(line)
+      end
+    end
+
+    def parse_line(line)
+      total_str, rest_str = line.split(": ")
+
+      total = total_str.to_i
+      operands = rest_str.split.map(&:to_i)
+
+      {
+        total: total,
+        operands: operands,
+      }
+    end
+
+    Calibration = Data.define(:total, :operands) do
+      def possible?
+        possible_operations.any? do |operation|
+          evaluate(operation) == total
+        end
+      end
+
+      def available_operators
+        %i[+ *]
+      end
+
+      def possible_operations
+        ops = available_operators.product
+        (operands.length - 2).times do
+          ops = ops.product(available_operators).map(&:flatten)
+        end
+
+        ops.map { |op_list| operands.zip(op_list).flatten.compact }
+      end
+
+      def evaluate(operation)
+        value = operation.first
+        idx = 1
+        while idx < operation.length
+          value = value.send(operation[idx], operation[idx + 1])
+          idx += 2
+        end
+        value
+      end
+    end
+
+    class Solver
+      def initialize(input)
+        @calibrations = input.map { |hash| Calibration.new(**hash) }
+      end
+
+      def total_calibration_result
+        # sum of the test values from just the equations that could possibly be true
+        @calibrations
+          .filter(&:possible?)
+          .map(&:total)
+          .sum
+      end
+    end
+
+    # ---
+
     def part_a
-      "PENDING-A"
+#       Solver.new(parse_input).total_calibration_result.to_s
+      # 7579994664753 in 28s
     end
 
     def part_b
--- 2024-ruby/spec/days/day07_spec.rb
+++ 2024-ruby/spec/days/day07_spec.rb
@@ -2,23 +2,76 @@
 
 require_relative "../../days/day07"
 
-RSpec.describe "Days::Day07" do
-  let(:day_runner) { Days::Day07.new(puzzle_input) }
+RSpec.describe Days::Day07 do
+  describe "DayRunner" do
+    let(:day_runner) { described_class.new(puzzle_input) }
 
-  let(:puzzle_input) { "" }
+    let(:puzzle_input) do
+      <<~INPUT
+        190: 10 19
+        3267: 81 40 27
+        83: 17 5
+        156: 15 6
+        7290: 6 8 6 15
+        161011: 16 10 13
+        192: 17 8 14
+        21037: 9 7 18 13
+        292: 11 6 16 20
+      INPUT
+    end
 
-  describe "#part_a" do
-    context "with the sample data" do
+    describe "#parse_input" do
+      it "returns an array of calibrations" do
+        expect(day_runner.parse_input).to eq(
+          [
+            { total: 190, operands: [10, 19] },
+            { total: 3267, operands: [81, 40, 27] },
+            { total: 83, operands: [17, 5] },
+            { total: 156, operands: [15, 6] },
+            { total: 7290, operands: [6, 8, 6, 15] },
+            { total: 161_011, operands: [16, 10, 13] },
+            { total: 192, operands: [17, 8, 14] },
+            { total: 21_037, operands: [9, 7, 18, 13] },
+            { total: 292, operands: [11, 6, 16, 20] },
+          ],
+        )
+      end
+    end
+
+    describe "#part_a" do
       it "returns the correct value for part A" do
-        expect(day_runner.part_a).to eq "PENDING-A"
+        expect(day_runner.part_a).to eq "3749"
+      end
+    end
+
+    describe "#part_b" do
+      it "returns the correct value for part B" do
+        expect(day_runner.part_b).to eq "PENDING-B"
       end
     end
   end
 
-  describe "#part_b" do
-    context "with the sample data" do
-      it "returns the correct value for part B" do
-        expect(day_runner.part_b).to eq "PENDING-B"
+  describe Days::Day07::Calibration do
+    [
+      {
+        calibration: described_class.new(total: 3267, operands: [81, 40, 27]),
+        operations: [
+          [81, :+, 40, :+, 27],
+          [81, :+, 40, :*, 27],
+          [81, :*, 40, :+, 27],
+          [81, :*, 40, :*, 27],
+        ],
+      },
+      {
+        calibration: described_class.new(total: 190, operands: [10, 19]),
+        operations: [
+          [10, :+, 19],
+          [10, :*, 19],
+        ],
+      },
+    ].each do |example|
+      it "creates the expect possible operations" do
+        expect(example[:calibration].possible_operations).to eq(example[:operations])
       end
     end
   end
