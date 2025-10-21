ea4327ebd92a76f6c53ca4894a80db59bc1d6224
Megan McVey <megan@offbyone.ca>
2025-10-21 14:52:09 -0300

    Day 09 Part A (Slow)

--- 2024-ruby/days/day09.rb
+++ 2024-ruby/days/day09.rb
@@ -6,8 +6,64 @@ module Days
       @puzzle_input = puzzle_input
     end
 
+    class DiskMap
+      def initialize(blocks)
+        @blocks = blocks
+      end
+
+      attr_reader :blocks
+
+      def checksum
+        @blocks.map.with_index { |char, idx| evaluate(char, idx) }.sum
+      end
+
+      def compact
+        space_blocks_count = blocks.count { it == "." }
+        correct_first_space_index = blocks.length - space_blocks_count
+
+        loop do
+          first_space_index = blocks.index(".")
+          break if first_space_index == correct_first_space_index
+
+          last_digit_index = blocks.rindex { it != "." }
+
+          # swap
+          blocks[first_space_index] = blocks[last_digit_index]
+          blocks[last_digit_index] = "."
+        end
+
+        self
+      end
+
+      private
+
+      def evaluate(char, idx)
+        return 0 if char == "."
+
+        char.to_i * idx
+      end
+
+      class << self
+        def from_representation(rep)
+          new(parse_blocks(rep))
+        end
+
+        def parse_blocks(rep)
+          blocks = []
+          "#{rep}0".chars.each_slice(2).with_index do |(file_size, space_size), file_id|
+            file_size.to_i.times { blocks << file_id }
+            space_size.to_i.times { blocks << "." }
+          end
+          blocks
+        end
+      end
+    end
+
     def part_a
-      "PENDING_A"
+      DiskMap.from_representation(puzzle_input)
+             .compact
+             .checksum.to_s
+      # 6346871685398 in 1m39s YIKES!
     end
 
     def part_b
--- 2024-ruby/spec/days/day09_spec.rb
+++ 2024-ruby/spec/days/day09_spec.rb
@@ -3,23 +3,38 @@
 require_relative "../../days/day09"
 
 RSpec.describe "Days::Day09" do
-  let(:day_runner) { Days::Day09.new(puzzle_input) }
+  describe "day_runner" do
+    let(:day_runner) { Days::Day09.new(puzzle_input) }
 
-  let(:puzzle_input) do
-    <<~INPUT
-      placeholder
-    INPUT
-  end
+    let(:puzzle_input) { "2333133121414131402" }
 
-  describe "#part_a" do
-    it "returns the correct value for part A" do
-      expect(day_runner.part_a).to eq "PENDING_A"
+    describe "#part_a" do
+      it "returns the correct value for part A" do
+        expect(day_runner.part_a).to eq "1928"
+      end
+    end
+
+    describe "#part_b" do
+      it "returns the correct value for part B" do
+        expect(day_runner.part_b).to eq "PENDING_B"
+      end
     end
   end
 
-  describe "#part_b" do
-    it "returns the correct value for part B" do
-      expect(day_runner.part_b).to eq "PENDING_B"
+  describe "DiskMap" do
+    let(:disk_map) { Days::Day09::DiskMap.from_representation("2333133121414131402") }
+
+    describe ".from_representation" do
+      it "parses the representation" do
+        expect(disk_map.blocks.join).to eq("00...111...2...333.44.5555.6666.777.888899")
+      end
+    end
+
+    describe ".compact" do
+      it "compacts the map" do
+        disk_map.compact
+        expect(disk_map.blocks.join).to eq("0099811188827773336446555566..............")
+      end
     end
   end
 end
