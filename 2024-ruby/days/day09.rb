ff3525ac345395835995e86f71a40646d5101102
Megan McVey <megan@offbyone.ca>
2025-10-21 18:49:40 -0300

    Day 09 Part B Improvement (37s)

--- 2024-ruby/days/day09.rb
+++ 2024-ruby/days/day09.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "forwardable"
+
 module Days
   class Day09
     def initialize(puzzle_input)
@@ -7,14 +9,18 @@ module Days
     end
 
     class StrategyBase
-      def initialize(blocks)
-        @blocks = blocks
+      def initialize(disk_map)
+        @disk_map = disk_map
       end
-
-      attr_reader :blocks
     end
 
-    class MoveFileBlocks < StrategyBase
+    class StrategyBlocksBase < StrategyBase
+      extend Forwardable
+
+      def_delegator :@disk_map, :blocks
+    end
+
+    class MoveFileBlocks < StrategyBlocksBase
       def compact
         space_blocks_count = blocks.count { it == "." }
         correct_first_space_index = blocks.length - space_blocks_count
@@ -34,7 +40,7 @@ module Days
       end
     end
 
-    class MoveWholeFiles < StrategyBase
+    class MoveWholeFiles < StrategyBlocksBase
       def compact
         last_file_block = blocks.rindex { it != "." }       # scan
         last_file_id = blocks[last_file_block].to_i
@@ -78,7 +84,7 @@ module Days
       end
     end
 
-    class MoveFileBlocksSmarter < StrategyBase
+    class MoveFileBlocksSmarter < StrategyBlocksBase
       def compact # rubocop:disable Metrics/MethodLength
         left = 0
         right = blocks.length - 1
@@ -95,10 +101,37 @@ module Days
       end
     end
 
-    File = Data.define(:index, :size, :id)
-    Free = Data.define(:index, :size)
+    File = Struct.new(:index, :size, :id, keyword_init: true) do # rubocop:disable Lint/StructNewOverride
+      def inspect
+        "ID#{id}(#{index},#{size})"
+      end
+
+      def to_s
+        inspect
+      end
+    end
+
+    Free = Struct.new(:index, :size, keyword_init: true) do # rubocop:disable Lint/StructNewOverride
+      def inspect
+        "F(#{index},#{size})"
+      end
+    end
+
     class MoveWholeFilesSmarter < StrategyBase
+      def initialize(disk_map)
+        super
+
+        @file_list = []
+        @free_list = {}
+
+        build_lists
+      end
+
       def compact
+        # representation: 2333133121414131402
+        # blocks-initial: 00...111...2...333.44.5555.6666.777.888899
+        # blocks-desired: 00992111777.44.333....5555.6666.....8888..
+
         # keep list of files, ordered by id
         # keep list of free spaces (bucketed by size), ordered by lowest index
         # so when looking for the earliest place to put a file of size N
@@ -108,6 +141,77 @@ module Days
         #   the Free we move into will shrink (and move lists)
         #   the Frees directly before and after the old File location will combine (and move lists)
         # building the FreeList and FileList requires the puzzle input instead of the blocks
+        @file_list.reverse.each do |file|
+          free = find_free(file.size, before: file.index)
+          next unless free
+
+          move(file, to: free)
+          build_blocks_for_checksum
+        end
+      end
+
+      def build_blocks_for_checksum
+        @disk_map.blocks.each_index { |idx| @disk_map.blocks[idx] = "." }
+        @file_list.each do |file|
+          file.size.times do |idx|
+            @disk_map.blocks[file.index + idx] = file.id
+          end
+        end
+
+        # puts @disk_map.blocks.join
+        # puts @file_list.join(" ")
+        # puts @free_list
+      end
+
+      def move(file, to:)
+        puts "MOVING ID#{file.id} from #{file.index} to #{to.index}"
+
+        file.index = to.index
+
+        @free_list[to.size].delete(to)
+
+        to.index = to.index + file.size
+        to.size -= file.size
+
+        @free_list[to.size] << to
+        @free_list[to.size].sort_by!(&:index)
+      end
+
+      def find_free(length, before:)
+        return nil if @free_list.keys.max < length
+
+        best_free = nil
+
+        (length..@free_list.keys.max).each do |n|
+          free = @free_list[n]&.first
+          next if free.nil?
+          next if free.index >= before
+
+          best_free = free if best_free.nil? || free.index < best_free.index
+        end
+
+        best_free
+      end
+
+      def build_lists # rubocop:disable Metrics/MethodLength
+        puts
+        next_is_space = false
+        idx = 0
+        file_id = 0
+        @disk_map.representation.chars.each do |char|
+          num = char.to_i
+          if next_is_space
+            @free_list[num] ||= []
+            @free_list[num] << Free.new(index: idx, size: num)
+          else
+            @file_list << File.new(index: idx, size: num, id: file_id)
+            file_id += 1
+          end
+          idx += num
+          next_is_space = !next_is_space
+        end
+
+        build_blocks_for_checksum
       end
     end
 
@@ -115,20 +219,18 @@ module Days
       def initialize(representation, strategy: MoveFileBlocks)
         @representation = representation
         @blocks = []
-        parse_blocks
         @strategy = strategy
+        parse_blocks
       end
 
-      attr_reader :blocks
+      attr_reader :blocks, :representation
 
       def checksum
-        @blocks.map.with_index { |char, idx| evaluate(char, idx) }.sum
+        blocks.map.with_index { |char, idx| evaluate(char, idx) }.sum
       end
 
       def compact
-        strategy = @strategy.new(@blocks)
-        strategy.compact
-        @blocks = strategy.blocks
+        @strategy.new(self).compact
 
         self
       end
@@ -157,14 +259,19 @@ module Days
       DiskMap.new(puzzle_input, strategy: MoveFileBlocksSmarter)
              .compact
              .checksum.to_s
-      # 6346871685398 instantaneous
+      # 6346871685398 instantaneous!!
     end
 
     def part_b
-      DiskMap.new(puzzle_input, strategy: MoveWholeFiles)
+      # DiskMap.new(puzzle_input, strategy: MoveWholeFiles)
+      #        .compact
+      #        .checksum.to_s
+      # 6373055193464 in 2m57s YIKES!
+      DiskMap.new(puzzle_input, strategy: MoveWholeFilesSmarter)
              .compact
              .checksum.to_s
-      # 6373055193464 in 2m57s YIKES!
+      # 6350673328412 (22381865052 too low) in 37s
+      # 6373055193464 in 37s
     end
 
     private
