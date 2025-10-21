ba0f7c80f878ce7045cc725a01cc2aaddbedc5a7
Megan McVey <megan@offbyone.ca>
2025-10-21 16:21:16 -0300

    Day 09 Part B (Slow)

--- 2024-ruby/days/day09.rb
+++ 2024-ruby/days/day09.rb
@@ -6,15 +6,11 @@ module Days
       @puzzle_input = puzzle_input
     end
 
-    class DiskMap
-      def initialize(blocks)
-        @blocks = blocks
-      end
-
+    class MoveFileBlocks
       attr_reader :blocks
 
-      def checksum
-        @blocks.map.with_index { |char, idx| evaluate(char, idx) }.sum
+      def initialize(blocks)
+        @blocks = blocks
       end
 
       def compact
@@ -31,6 +27,81 @@ module Days
           blocks[first_space_index] = blocks[last_digit_index]
           blocks[last_digit_index] = "."
         end
+      end
+    end
+
+    class MoveWholeFiles
+      attr_reader :blocks
+
+      def initialize(blocks)
+        @blocks = blocks
+      end
+
+      def compact
+        last_file_block = blocks.rindex { it != "." }
+        last_file_id = blocks[last_file_block].to_i
+        last_file_id.downto(0) do |file_id|
+          try_move_file(file_id)
+        end
+      end
+
+      def try_move_file(file_id)
+        start_of_file = blocks.index(file_id)
+        end_of_file = blocks.rindex(file_id)
+
+        return if start_of_file.nil?
+
+        file_size = end_of_file - start_of_file + 1
+        target = find_space(file_size, before: start_of_file)
+
+        movable = target || "NOMOVE"
+        puts "#{file_id}: [#{start_of_file}+#{file_size}] -->#{movable}"
+
+        return if target.nil?
+
+        move_file(file_id, start_of_file, file_size, target)
+      end
+
+      def move_file(file_id, start_of_file, file_size, target)
+        file_size.times do |offset|
+          blocks[target + offset] = file_id
+          blocks[start_of_file + offset] = "."
+        end
+      end
+
+      def find_space(spaces, before:)
+        idx = 0
+        loop do
+          return nil if idx >= before
+          return idx if idx_valid?(idx, spaces)
+
+          idx += 1
+        end
+      end
+
+      def idx_valid?(idx, spaces)
+        (0...spaces).all? { |spc_idx| blocks[idx + spc_idx] == "." }
+      end
+    end
+
+    class DiskMap
+      def initialize(representation, strategy: MoveFileBlocks)
+        @representation = representation
+        @blocks = []
+        parse_blocks
+        @strategy = strategy
+      end
+
+      attr_reader :blocks
+
+      def checksum
+        @blocks.map.with_index { |char, idx| evaluate(char, idx) }.sum
+      end
+
+      def compact
+        strategy = @strategy.new(@blocks)
+        strategy.compact
+        @blocks = strategy.blocks
 
         self
       end
@@ -43,31 +114,26 @@ module Days
         char.to_i * idx
       end
 
-      class << self
-        def from_representation(rep)
-          new(parse_blocks(rep))
-        end
-
-        def parse_blocks(rep)
-          blocks = []
-          "#{rep}0".chars.each_slice(2).with_index do |(file_size, space_size), file_id|
-            file_size.to_i.times { blocks << file_id }
-            space_size.to_i.times { blocks << "." }
-          end
-          blocks
+      def parse_blocks
+        "#{@representation}0".chars.each_slice(2).with_index do |(file_size, space_size), file_id|
+          file_size.to_i.times { blocks << file_id }
+          space_size.to_i.times { blocks << "." }
         end
       end
     end
 
     def part_a
-      DiskMap.from_representation(puzzle_input)
+      DiskMap.new(puzzle_input)
              .compact
              .checksum.to_s
       # 6346871685398 in 1m39s YIKES!
     end
 
     def part_b
-      "PENDING_B"
+      DiskMap.new(puzzle_input, strategy: MoveWholeFiles)
+             .compact
+             .checksum.to_s
+      # 6373055193464 in 2m57s YIKES!
     end
 
     private
--- 2024-ruby/spec/days/day09_spec.rb
+++ 2024-ruby/spec/days/day09_spec.rb
@@ -16,15 +16,16 @@ RSpec.describe "Days::Day09" do
 
     describe "#part_b" do
       it "returns the correct value for part B" do
-        expect(day_runner.part_b).to eq "PENDING_B"
+        expect(day_runner.part_b).to eq "2858"
       end
     end
   end
 
   describe "DiskMap" do
-    let(:disk_map) { Days::Day09::DiskMap.from_representation("2333133121414131402") }
+    let(:strategy) { Days::Day09::MoveFileBlocks }
+    let(:disk_map) { Days::Day09::DiskMap.new("2333133121414131402", strategy: strategy) }
 
-    describe ".from_representation" do
+    describe "initial state" do
       it "parses the representation" do
         expect(disk_map.blocks.join).to eq("00...111...2...333.44.5555.6666.777.888899")
       end
@@ -34,6 +35,15 @@ RSpec.describe "Days::Day09" do
       it "compacts the map" do
         disk_map.compact
         expect(disk_map.blocks.join).to eq("0099811188827773336446555566..............")
+      end
+
+      context "when in part b" do # rubocop:disable RSpec/NestedGroups
+        let(:strategy) { Days::Day09::MoveWholeFiles }
+
+        it "compacts the map differently" do
+          disk_map.compact
+          expect(disk_map.blocks.join).to eq("00992111777.44.333....5555.6666.....8888..")
+        end
       end
     end
   end
