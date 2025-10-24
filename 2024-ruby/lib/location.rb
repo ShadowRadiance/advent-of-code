e48ab493b7158536ba11eef5a0f74fbd3bc4d0fa
Megan McVey <megan@offbyone.ca>
2025-10-24 18:18:25 -0300

    >> Location adjacency

--- 2024-ruby/lib/location.rb
+++ 2024-ruby/lib/location.rb
@@ -10,4 +10,29 @@ class Location < Vector
   def inspect
     "@(#{x},#{y})"
   end
+
+  def adjacent?(other)
+    cardinally_adjacent?(other) || diagonally_adjacent?(other)
+  end
+
+  def diagonally_adjacent?(other)
+    (other.x == x + 1 && (other.y == y + 1 || other.y == y - 1)) ||
+      (other.x == x - 1 && (other.y == y + 1 || other.y == y - 1))
+  end
+
+  def horizontally_adjacent?(other)
+    return false if other.y != y
+
+    other.x == x + 1 || other.x == x - 1
+  end
+
+  def vertically_adjacent?(other)
+    return false if other.x != x
+
+    other.y == y + 1 || other.y == y - 1
+  end
+
+  def cardinally_adjacent?(other)
+    horizontally_adjacent?(other) || vertically_adjacent?(other)
+  end
 end
