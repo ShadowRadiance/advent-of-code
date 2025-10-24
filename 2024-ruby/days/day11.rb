459ad34551a5c0045e012d9b5ad73bbb1c4c95c8
Megan McVey <megan@offbyone.ca>
2025-10-23 22:57:48 -0300

    Day 11 Part A

--- 2024-ruby/days/day11.rb
+++ 2024-ruby/days/day11.rb
@@ -6,12 +6,60 @@ module Days
       @puzzle_input = puzzle_input
     end
 
+    class Solution
+      attr_reader :stones
+
+      def initialize(stones)
+        @stones = stones
+      end
+
+      def blink(times)
+        times.times do
+          @stones.map! { |stone| blink_stone(stone) }
+          @stones.flatten!
+        end
+      end
+
+      def blink_stone(stone)
+        return 1 if stone.zero?
+
+        return split(stone) if stone.to_s.length.even?
+
+        stone * 2024
+      end
+
+      def split(stone)
+        half_length = stone.to_s.length / 2
+        divisor = 10.pow(half_length)
+        original_value = stone
+        stone = original_value / divisor
+        new_stone = original_value % divisor
+
+        [
+          stone,
+          new_stone,
+        ]
+      end
+    end
+
     def part_a
-      "PENDING_A"
+      solution = Solution.new(parse_input)
+      solution.blink(25)
+      solution.stones.size.to_s
+      # 199982 instant
     end
 
     def part_b
-      "PENDING_B"
+      # should've seen this coming
+      # too much array shuffling when we get this big
+      solution = Solution.new(parse_input)
+      solution.blink(75)
+      solution.stones.size.to_s
+      # too slow
+    end
+
+    def parse_input
+      puzzle_input.split.map(&:to_i)
     end
 
     private
--- 2024-ruby/spec/days/day11_spec.rb
+++ 2024-ruby/spec/days/day11_spec.rb
@@ -5,15 +5,11 @@ require_relative "../../days/day11"
 RSpec.describe "Days::Day11" do
   let(:day_runner) { Days::Day11.new(puzzle_input) }
 
-  let(:puzzle_input) do
-    <<~INPUT
-      placeholder
-    INPUT
-  end
+  let(:puzzle_input) { "125 17" }
 
   describe "#part_a" do
     it "returns the correct value for part A" do
-      expect(day_runner.part_a).to eq "PENDING_A"
+      expect(day_runner.part_a).to eq "55312"
     end
   end
 
