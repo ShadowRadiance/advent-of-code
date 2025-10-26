8c0e78db19831e69d2633bb3b2f3f4d30bad6a85
Megan McVey <megan@offbyone.ca>
2025-10-26 14:17:19 -0300

    Day 14 Part B -- Image Recognition??

--- 2024-ruby/days/day14.rb
+++ 2024-ruby/days/day14.rb
@@ -86,6 +86,15 @@ module Days
     end
 
     def part_b
+      # During the bathroom break, someone notices that these robots seem
+      # awfully similar to ones built and used at the North Pole. If they're
+      # the same type of robots, they should have a hard-coded Easter egg:
+      # very rarely, most of the robots should arrange themselves into a
+      # picture of a Christmas tree.
+      # What is the fewest number of seconds that must elapse for the robots to display the Easter egg?
+      #
+      # WAT? How do we know when it's christmas tree enough???
+
       "PENDING_B"
     end
 
