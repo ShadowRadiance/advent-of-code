e512e435e7eebf065b97b1c2486e23559b361235
Megan McVey <megan@offbyone.ca>
2025-10-26 12:09:52 -0300

    Day 13 Part A

--- 2024-ruby/days/day13.rb
+++ 2024-ruby/days/day13.rb
@@ -1,13 +1,104 @@
 # frozen_string_literal: true
 
+require "matrix"
+
+require_relative "../lib/aoc/direction"
+require_relative "../lib/aoc/location"
+
 module Days
   class Day13
+    class MachineBuilder
+      BUTTON_REGEX = /Button (?:A|B): X\+(\d+), Y\+(\d+)/
+      PRIZE_REGEX = /Prize: X=(\d+), Y=(\d+)/
+
+      def build_machine_from_description(button_a_desc, button_b_desc, prize_desc)
+        matches_a = BUTTON_REGEX.match(button_a_desc)
+        matches_b = BUTTON_REGEX.match(button_b_desc)
+        matches_p = PRIZE_REGEX.match(prize_desc)
+        Machine.new(
+          button_a: direction_from(*matches_a[1..]),
+          button_b: direction_from(*matches_b[1..]),
+          prize: location_from(*matches_p[1..]),
+        )
+      end
+
+      def direction_from(xxx, yyy) = AOC::Direction.new(x: xxx.to_i, y: yyy.to_i)
+      def location_from(xxx, yyy) = AOC::Location.new(x: xxx.to_i, y: yyy.to_i)
+    end
+
+    Machine = Data.define(:button_a, :button_b, :prize) do
+      def cheapest_prize # rubocop:disable Metrics/AbcSize
+        # solve for n and m: {
+        #   n * button_a.x + m * button_b.x == prize.x
+        #   n * button_a.y + m * button_b.y == prize.y
+        # }
+        #
+        # example:
+        #         Button A: X+94, Y+34
+        #         Button B: X+22, Y+67
+        #         Prize: X=8400, Y=5400
+        # {
+        #   94n + 22m = 8400
+        #   34n + 67m = 5400
+        # }
+        #
+        # Matrix Substitution
+        # |94 22| |n|=|8400| ==> |94 22 8400| P
+        # |34 67| |m|=|5400| ==> |34 67 5400| Q
+        #
+        # Q=34P-94Q
+        #   ==> |94    22    8400|
+        #   ==> | 0 -5550 -222000|
+        # Q=Q/-5550
+        #   ==> |94 22 8400|
+        #   ==> | 0  1   40|
+        # P=P-22Q
+        #   ==> |94 0 7520|
+        #   ==> | 0 1   40|
+        # P=P/94
+        #   ==> |1  0  80|
+        #   ==> |0  1  40|
+        #
+        # n = 80, m = 40
+        #
+        # Inverse of a Matrix Version
+        #
+        # |94 22|*|n|=|8400|   or (B*C=P) thus C=B.inverse*P
+        # |34 67| |m| |5400|
+
+        matrix_buttons = ::Matrix[
+          [button_a.x, button_b.x],
+          [button_a.y, button_b.y],
+        ]
+
+        matrix_prize = Matrix[[prize.x], [prize.y]]
+
+        matrix_counts = matrix_buttons.inverse * matrix_prize
+
+        a_count = matrix_counts[0, 0]
+        b_count = matrix_counts[1, 0]
+        # non-integer values imply it takes a fraction of a button push
+        # therefore no solution
+        return nil unless a_count.to_i == a_count
+        return nil unless b_count.to_i == b_count
+
+        (3 * a_count.to_i) + b_count.to_i
+      end
+    end
+
     def initialize(puzzle_input)
       @puzzle_input = puzzle_input
     end
 
+    def parse_input
+      puzzle_input.split("\n\n").map do |desc|
+        MachineBuilder.new.build_machine_from_description(*desc.lines(chomp: true))
+      end
+    end
+
     def part_a
-      "PENDING_A"
+      machines = parse_input
+      machines.map(&:cheapest_prize).compact.sum.to_s
     end
 
     def part_b
--- 2024-ruby/spec/days/day13_spec.rb
+++ 2024-ruby/spec/days/day13_spec.rb
@@ -7,13 +7,79 @@ RSpec.describe "Days::Day13" do
 
   let(:puzzle_input) do
     <<~INPUT
-      placeholder
+      Button A: X+94, Y+34
+      Button B: X+22, Y+67
+      Prize: X=8400, Y=5400
+
+      Button A: X+26, Y+66
+      Button B: X+67, Y+21
+      Prize: X=12748, Y=12176
+
+      Button A: X+17, Y+86
+      Button B: X+84, Y+37
+      Prize: X=7870, Y=6450
+
+      Button A: X+69, Y+23
+      Button B: X+27, Y+71
+      Prize: X=18641, Y=10279
     INPUT
+  end
+
+  describe "Machine" do
+    it "== compares by ivar" do
+      a = Days::Day13::Machine.new(
+        button_a: AOC::Direction.new(1, 2),
+        button_b: AOC::Direction.new(3, 4),
+        prize: AOC::Location.new(5, 6),
+      )
+      b = Days::Day13::Machine.new(
+        button_a: AOC::Direction.new(1, 2),
+        button_b: AOC::Direction.new(3, 4),
+        prize: AOC::Location.new(5, 6),
+      )
+      expect(a == b).to be(true)
+    end
+
+    describe "#cheapest_prize" do
+      it do
+        machines = day_runner.parse_input
+        expect(machines.map(&:cheapest_prize)).to eq([280, nil, 200, nil])
+      end
+    end
+  end
+
+  describe "#parse_input" do
+    it "returns an array of machines" do
+      expect(day_runner.parse_input).to eq(
+        [
+          Days::Day13::Machine.new(
+            button_a: AOC::Direction.new(x: 94, y: 34),
+            button_b: AOC::Direction.new(x: 22, y: 67),
+            prize: AOC::Location.new(x: 8400, y: 5400),
+          ),
+          Days::Day13::Machine.new(
+            button_a: AOC::Direction.new(x: 26, y: 66),
+            button_b: AOC::Direction.new(x: 67, y: 21),
+            prize: AOC::Location.new(x: 12_748, y: 12_176),
+          ),
+          Days::Day13::Machine.new(
+            button_a: AOC::Direction.new(x: 17, y: 86),
+            button_b: AOC::Direction.new(x: 84, y: 37),
+            prize: AOC::Location.new(x: 7870, y: 6450),
+          ),
+          Days::Day13::Machine.new(
+            button_a: AOC::Direction.new(x: 69, y: 23),
+            button_b: AOC::Direction.new(x: 27, y: 71),
+            prize: AOC::Location.new(x: 18_641, y: 10_279),
+          ),
+        ],
+      )
+    end
   end
 
   describe "#part_a" do
     it "returns the correct value for part A" do
-      expect(day_runner.part_a).to eq "PENDING_A"
+      expect(day_runner.part_a).to eq "480"
     end
   end
 
