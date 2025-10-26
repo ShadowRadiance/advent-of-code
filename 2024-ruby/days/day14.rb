97f62b8586b5fd8c9babf8e6df543c07c9f2287f
Megan McVey <megan@offbyone.ca>
2025-10-26 14:16:28 -0300

    Day 14 Part A

--- 2024-ruby/days/day14.rb
+++ 2024-ruby/days/day14.rb
@@ -1,13 +1,88 @@
 # frozen_string_literal: true
 
+require_relative "../lib/aoc/grid"
+require_relative "../lib/aoc/bounds"
+require_relative "../lib/aoc/location"
+require_relative "../lib/aoc/direction"
+
 module Days
   class Day14
     def initialize(puzzle_input)
       @puzzle_input = puzzle_input
     end
 
-    def part_a
-      "PENDING_A"
+    LINE_PARSER = /p=(\d+),(\d+)\ v=(-?\d+),(-?\d+)/
+    Robot = Data.define(:location, :direction)
+
+    def robot_from_description(line)
+      # p=0,4 v=3,-3
+      matches = LINE_PARSER.match(line)
+      Robot.new(
+        AOC::Location.new(x: matches[1].to_i, y: matches[2].to_i),
+        AOC::Direction.new(x: matches[3].to_i, y: matches[4].to_i),
+      )
+    end
+
+    def parse_input
+      puzzle_input.lines(chomp: true).map { robot_from_description(it) }
+    end
+
+    def teleport_wrap(location)
+      AOC::Location.new(location.x % @max_x, location.y % @max_y)
+    end
+
+    def walk_robot(robot, times)
+      Robot.new(
+        location: teleport_wrap(robot.location + (robot.direction * times)),
+        direction: robot.direction,
+      )
+    end
+
+    def count_robots_in_quadrant(quadrant, robots)
+      robots.count { it.location.in_bounds?(quadrant) }
+    end
+
+    def quadrants
+      half_x = (@max_x - 1) / 2 # 11 => 5
+      half_y = (@max_y - 1) / 2 #  7 => 3
+
+      [
+        AOC::Bounds.new(
+          min_x: 0,
+          max_x: half_x - 1,
+          min_y: 0,
+          max_y: half_y - 1,
+        ),
+        AOC::Bounds.new(
+          min_x: half_x + 1,
+          max_x: @max_x,
+          min_y: 0,
+          max_y: half_y - 1,
+        ),
+        AOC::Bounds.new(
+          min_x: 0,
+          max_x: half_x - 1,
+          min_y: half_y + 1,
+          max_y: @max_y,
+        ),
+        AOC::Bounds.new(
+          min_x: half_x + 1,
+          max_x: @max_x,
+          min_y: half_y + 1,
+          max_y: @max_y,
+        ),
+      ]
+    end
+
+    def part_a(testing: false)
+      @max_x = testing ? 11 : 101
+      @max_y = testing ? 7 : 103
+      robots = parse_input
+      robots = robots.map { |robot| walk_robot(robot, 100) }
+
+      quadrants.map { count_robots_in_quadrant(it, robots) }
+               .reduce(:*)
+               .to_s
     end
 
     def part_b
--- 2024-ruby/spec/days/day14_spec.rb
+++ 2024-ruby/spec/days/day14_spec.rb
@@ -7,13 +7,45 @@ RSpec.describe "Days::Day14" do
 
   let(:puzzle_input) do
     <<~INPUT
-      placeholder
+      p=0,4 v=3,-3
+      p=6,3 v=-1,-3
+      p=10,3 v=-1,2
+      p=2,0 v=2,-1
+      p=0,0 v=1,3
+      p=3,0 v=-2,-2
+      p=7,6 v=-1,-3
+      p=3,0 v=-1,-2
+      p=9,3 v=2,3
+      p=7,3 v=-1,2
+      p=2,4 v=2,-3
+      p=9,5 v=-3,-3
     INPUT
+  end
+
+  describe "parsing" do
+    it "parses the input into robots" do
+      expect(day_runner.parse_input).to eq(
+        [
+          Days::Day14::Robot.new(AOC::Location.new(0, 4), AOC::Direction.new(3, -3)),
+          Days::Day14::Robot.new(AOC::Location.new(6, 3), AOC::Direction.new(-1, -3)),
+          Days::Day14::Robot.new(AOC::Location.new(10, 3), AOC::Direction.new(-1, 2)),
+          Days::Day14::Robot.new(AOC::Location.new(2, 0), AOC::Direction.new(2, -1)),
+          Days::Day14::Robot.new(AOC::Location.new(0, 0), AOC::Direction.new(1, 3)),
+          Days::Day14::Robot.new(AOC::Location.new(3, 0), AOC::Direction.new(-2, -2)),
+          Days::Day14::Robot.new(AOC::Location.new(7, 6), AOC::Direction.new(-1, -3)),
+          Days::Day14::Robot.new(AOC::Location.new(3, 0), AOC::Direction.new(-1, -2)),
+          Days::Day14::Robot.new(AOC::Location.new(9, 3), AOC::Direction.new(2, 3)),
+          Days::Day14::Robot.new(AOC::Location.new(7, 3), AOC::Direction.new(-1, 2)),
+          Days::Day14::Robot.new(AOC::Location.new(2, 4), AOC::Direction.new(2, -3)),
+          Days::Day14::Robot.new(AOC::Location.new(9, 5), AOC::Direction.new(-3, -3)),
+        ],
+      )
+    end
   end
 
   describe "#part_a" do
     it "returns the correct value for part A" do
-      expect(day_runner.part_a).to eq "PENDING_A"
+      expect(day_runner.part_a(testing: true)).to eq "12"
     end
   end
 
