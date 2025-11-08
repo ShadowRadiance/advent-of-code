055108414fc2b60c4a63b889dea0d00bef3feee9
Megan McVey <megan@offbyone.ca>
2025-11-08 08:33:46 -0400

    Day 18 Setup

---
+++ 2024-ruby/days/day18.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Days
+  class Day18
+    def initialize(puzzle_input)
+      @puzzle_input = puzzle_input
+    end
+
+    def part_a
+      "PENDING_A"
+    end
+
+    def part_b
+      "PENDING_B"
+    end
+
+    private
+
+    attr_reader :puzzle_input
+  end
+end
