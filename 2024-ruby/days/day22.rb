7d8c17322785c53820cd7ba3c561bd9b7ed62e5d
Megan McVey <megan@offbyone.ca>
2025-11-23 09:18:07 -0400

    Day 22 Part A

--- 2024-ruby/days/day22.rb
+++ 2024-ruby/days/day22.rb
@@ -7,15 +7,56 @@ module Days
     end
 
     def part_a
-      "PENDING_A"
+      results = @puzzle_input.lines(chomp: true).map do |line|
+        SecretNumberGenerator.new(base: line.to_i).generate_next_n(2000)
+      end
+      results.sum.to_s
+      # 18317943467 in 0.8s
     end
 
     def part_b
       "PENDING_B"
     end
 
-    private
+    class SecretNumberGenerator
+      def initialize(base:)
+        @base = base
+        @current = base
+      end
 
-    attr_reader :puzzle_input
+      PRIMARY   = 64   # = 0b0000_0100_0000 # X * P === X<<6
+      SECONDARY = 32   # = 0b0000_0010_0000 # X / S === X>>5
+      TERTIARY  = 2048 # = 0b1000_0000_0000 # X * T === X<<11
+
+      def generate_next
+        secret = @current
+        # secret * primary === secret << 6
+        secret = prune(mix(secret, secret * PRIMARY))
+        # secret / secondary === secret >> 5
+        secret = prune(mix(secret, secret / SECONDARY))
+        # secret * tertiary === secret << 11
+        secret = prune(mix(secret, secret * TERTIARY))
+        @current = secret
+      end
+
+      def generate_next_n(_n)
+        2000.times { generate_next }
+        @current
+      end
+
+      def mix(xxx, yyy)
+        xxx ^ yyy # bitwise xor
+
+        # since we always prune after we mix, only the last 24 bits matter
+      end
+
+      def prune(xxx)
+        xxx % 16_777_216
+        # 16_777_216 decimal
+        # = 0x1000000 hex
+        # = 0b0000_0001_0000_0000_0000_0000_0000_0000 binary
+        # i.e. "keep the last 24 bits"
+      end
+    end
   end
 end
--- 2024-ruby/spec/days/day22_spec.rb
+++ 2024-ruby/spec/days/day22_spec.rb
@@ -3,23 +3,67 @@
 require_relative "../../days/day22"
 
 RSpec.describe "Days::Day22" do
-  let(:day_runner) { Days::Day22.new(puzzle_input) }
+  describe "Secret Number Generator" do
+    let(:sng) { Days::Day22::SecretNumberGenerator.new(base: base) }
 
-  let(:puzzle_input) do
-    <<~INPUT
-      placeholder
-    INPUT
-  end
+    context "with a base of 123" do
+      let(:base) { 123 }
 
-  describe "#part_a" do
-    it "returns the correct value for part A" do
-      expect(day_runner.part_a).to eq "PENDING_A"
+      it "yields the expected numbers" do
+        results = (1..10).map { sng.generate_next }
+        expect(results).to contain_exactly(
+          15_887_950,
+          16_495_136,
+          527_345,
+          704_524,
+          1_553_684,
+          12_683_156,
+          11_100_544,
+          12_249_484,
+          7_753_432,
+          5_908_254,
+        )
+      end
+    end
+
+    {
+      1 => 8_685_429,
+      10 => 4_700_978,
+      100 => 15_273_692,
+      2024 => 8_667_524,
+    }.each do |k, v|
+      context "with a base of #{k}" do
+        let(:base) { k }
+
+        it "yields #{v} as the 2000th number" do
+          expect(sng.generate_next_n(2000)).to eq(v)
+        end
+      end
     end
   end
 
-  describe "#part_b" do
-    it "returns the correct value for part B" do
-      expect(day_runner.part_b).to eq "PENDING_B"
+  describe "Main Problem" do
+    let(:day_runner) { Days::Day22.new(puzzle_input) }
+
+    let(:puzzle_input) do
+      <<~INPUT
+        1
+        10
+        100
+        2024
+      INPUT
+    end
+
+    describe "#part_a" do
+      it "returns the correct value for part A" do
+        expect(day_runner.part_a).to eq "37327623"
+      end
+    end
+
+    describe "#part_b" do
+      it "returns the correct value for part B" do
+        expect(day_runner.part_b).to eq "PENDING_B"
+      end
     end
   end
 end
